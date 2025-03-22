23ec2e03adeadeaa43d51d9149d44dd4a7c7f493
sid314 <sidaa314@gmail.com>
2025-03-22 06:46:43 +0530

    Reduced further chonk

--- app.go
+++ app.go
@@ -12,15 +12,15 @@ import (
 
 func main() {
 	const (
-		yellow = "\033[93m"
-		red    = "\033[91m"
-		green  = "\033[92m"
-		blue   = "\033[94m"
-		pink   = "\033[95m"
-		teal   = "\033[96m"
-		grey   = "\033[97m"
-		black  = "\033[30m"
-		stop   = "\033[0m"
+		yellow = "\033[93m\b"
+		red    = "\033[91m\b"
+		green  = "\033[92m\b"
+		blue   = "\033[94m\b"
+		pink   = "\033[95m\b"
+		teal   = "\033[96m\b"
+		grey   = "\033[97m\b"
+		black  = "\033[30m\b"
+		stop   = "\033[0\bm"
 	)
 	hostname, _ := os.Hostname()
 	user, _ := user.Current()
@@ -48,16 +48,16 @@ func main() {
 	freememinmebibytes := freememinkilobytes / 1024
 	usedmeminmebibytes := totalmeminmebibytes - freememinmebibytes
 	dot := ""
-	fmt.Println("╭─────────────╮")
+	fmt.Println("╭───────────╮")
 	fmt.Println("│", yellow, " ", stop, "user   │", red, username, stop)
 	fmt.Println("│", yellow, " ", stop, "hname  │", yellow, hostname, stop)
 	fmt.Println("│", green, " ", stop, "distro │", green, distroname[1], stop)
-	fmt.Print("│", green, "  󰌢 ", stop, "  kernel │  ", blue, kernel, stop)
-	fmt.Print("│", blue, "   ", stop, "  uptime │  ", red, uptime, stop)
+	fmt.Print("│ ", green, " 󰌢 ", stop, "  kernel │  ", blue, kernel, stop)
+	fmt.Print("│ ", blue, "  ", stop, "  uptime │  ", red, uptime, stop)
 	fmt.Println("│", blue, " ", stop, "shell  │", yellow, shell, stop)
-	fmt.Print("│", red, "  󰏖 ", stop, "  pkgs   │  ", green, packages, stop)
+	fmt.Print("│ ", red, " 󰏖 ", stop, "  pkgs   │  ", green, packages, stop)
 	fmt.Println("│", red, "󰍛 ", stop, "mem    │", blue, usedmeminmebibytes, "|", totalmeminmebibytes, "MiB", stop)
-	fmt.Println("├─────────────┤")
-	fmt.Println("│  󰏘  colors  │", grey, dot, red, dot, yellow, dot, green, dot, teal, dot, blue, dot, pink, dot, black, dot, stop)
-	fmt.Println("╰─────────────╯")
+	fmt.Println("├───────────┤")
+	fmt.Println("│ 󰏘  colors │", grey, dot, red, dot, yellow, dot, green, dot, teal, dot, blue, dot, pink, dot, black, dot, stop)
+	fmt.Println("╰───────────╯")
 }
