fc6b7b8d78ae689de83772a05e6def13c548660c
sid314 <sidaa314@gmail.com>
2025-03-31 11:45:32 +0530

    Started work on themes

--- help.go
+++ help.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"strings"
+
+	catppuccin "github.com/catppuccin/go"
+	"github.com/fatih/color"
 )
 
 func SnipSnip(prefix string, suffix string, s string) string {
 	_, aftercut, _ := strings.Cut(s, prefix)
 	beforecut, _, _ := strings.Cut(aftercut, suffix)
 	return beforecut
+}
+
+func catppuccinToColor(namedColor catppuccin.Color) *color.Color {
+	r, g, b, _ := namedColor.RGBA()
+	R, G, B := int(r), int(g), int(b)
+	c := color.RGB(R, G, B)
+	return c
 }
