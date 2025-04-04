ed839d3a44fb330e9a41a6b88a1c1ccb2e113b57
sid314 <sidaa314@gmail.com>
2025-04-04 15:49:53 +0530

    Finalised printing logic

--- help.go
+++ help.go
@@ -21,7 +21,65 @@ func catppuccinToColor(namedColor catppuccin.Color) *color.Color {
 	return color
 }
 
+func GeneratePrintFunctions(colors Palette) PrintFunctions {
+	var functions [16]func(a ...any) string
+	for i := range colors {
+		functions[i] = colors[i].SprintFunc()
+	}
+	return functions
+}
+
+func GeneratePalette(theme ThemeName) Palette {
+	switch theme {
+	case "catppuccin-mocha", "catppuccin-latte", "catppuccin-frappe", "catppuccin-macchiato":
+		return generateCatpuccinPalette(theme)
+	default:
+		return generateGrayscalePalette()
+
+	}
+}
+
 func PrintConfig(config Config) {
-	fmt.Println(config.style)
-	fmt.Println(config.theme)
+	fmt.Println(config.Style)
+	fmt.Println(config.Theme)
+}
+
+func generateGrayscalePalette() Palette {
+	var palette Palette
+	for i := range palette {
+		palette[i] = color.RGB(255, 255, 255)
+	}
+	return palette
+}
+
+func generateCatpuccinPalette(theme ThemeName) Palette {
+	var flavour catppuccin.Flavor
+	var palette Palette
+	switch theme {
+	case "catppuccin-mocha":
+		flavour = catppuccin.Mocha
+	case "catppuccin-macchiato":
+		flavour = catppuccin.Macchiato
+	case "catppuccin-frappe":
+		flavour = catppuccin.Frappe
+	case "catppuccin-latte":
+		flavour = catppuccin.Latte
+	}
+	palette[0] = catppuccinToColor(flavour.Sky())
+	palette[1] = catppuccinToColor(flavour.Sapphire())
+	palette[2] = catppuccinToColor(flavour.Lavender())
+	palette[3] = catppuccinToColor(flavour.Mauve())
+	palette[4] = catppuccinToColor(flavour.Blue())
+	palette[5] = catppuccinToColor(flavour.Sky())
+	palette[6] = catppuccinToColor(flavour.Peach())
+	palette[7] = catppuccinToColor(flavour.Yellow())
+	palette[8] = catppuccinToColor(flavour.Red())
+	palette[9] = catppuccinToColor(flavour.Maroon())
+	palette[10] = catppuccinToColor(flavour.Green())
+	palette[11] = catppuccinToColor(flavour.Teal())
+	palette[12] = catppuccinToColor(flavour.Rosewater())
+	palette[13] = catppuccinToColor(flavour.Flamingo())
+	palette[14] = catppuccinToColor(flavour.Pink())
+	palette[15] = catppuccinToColor(flavour.Lavender())
+	return palette
 }
--- print.go
+++ print.go
@@ -1,12 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+// By default palettes contain 16 colors. If a theme has any less, the remaining are set to white
+type (
+	Palette        [16]*color.Color
+	PrintFunctions [16]func(a ...any) string
+)
+
+type (
+	StyleName string
+	ThemeName string
+	Theme     struct {
+		name   ThemeName
+		colors Palette
+	}
+)
 
 func Print() {
 	config := GetConfig()
-	switch config.style {
+	switch config.Style {
 	case "nitch":
-		printNitch(config.theme)
+		printNitch(config.Theme)
 	case "classic":
 		// printClassic(config.theme)
 	}
@@ -22,21 +41,24 @@ func Print() {
 	// fmt.Println("╰───────────╯")
 }
 
-func printNitch(theme string) {
+func printNitch(theme ThemeName) {
 	info := GetInfo()
-	// dot := ""
-	if theme == "grayscale" {
-		fmt.Printf("╭───────────╮\n")
-		fmt.Printf("│   user   │ %s \n", info.username)
-		fmt.Printf("│   host   │ %s \n", info.hostname)
-		fmt.Printf("│   distro │ %s \n", info.distro)
-		fmt.Printf("│ 󰌢  kernel │ %s \n", info.kernel)
-		fmt.Printf("│   uptime │ %s \n", info.uptime)
-		fmt.Printf("│   shell  │ %s \n", info.shell)
-		fmt.Printf("│ 󰏖  pkgs   │ %d \n", info.packages)
-		fmt.Printf("│ 󰍛  memory │ %d | %d \n", info.usedMemory, info.totalMemory)
-		fmt.Printf("├───────────┤ \n")
-		fmt.Printf("│ 󰏘  colors │         \n")
-		fmt.Printf("╰───────────╯\n")
+	dot := ""
+	printFunctions := GeneratePrintFunctions(GeneratePalette(theme))
+	fmt.Printf("╭───────────╮\n")
+	fmt.Printf("│ %s   │ %s \n", printFunctions[0]("  user"), printFunctions[1](info.username))
+	fmt.Printf("│ %s   │ %s \n", printFunctions[2]("  host"), printFunctions[3](info.hostname))
+	fmt.Printf("│ %s │ %s \n", printFunctions[4]("  distro"), printFunctions[5](info.distro))
+	fmt.Printf("│ %s │ %s \n", printFunctions[6]("󰌢  kernel"), printFunctions[7](info.kernel))
+	fmt.Printf("│ %s │ %s \n", printFunctions[8]("  uptime"), printFunctions[9](info.uptime))
+	fmt.Printf("│ %s  │ %s \n", printFunctions[10]("  shell"), printFunctions[11](info.shell))
+	fmt.Printf("│ %s   │ %s \n", printFunctions[12]("󰏖  pkgs"), printFunctions[13](info.packages))
+	fmt.Printf("│ %s │ %s | %s \n", printFunctions[14]("󰍛  memory"), printFunctions[15](info.usedMemory), printFunctions[15](info.totalMemory))
+	fmt.Printf("├───────────┤ \n")
+	fmt.Printf("│ %s │", printFunctions[0]("󰏘  colors"))
+	for i := range 8 {
+		fmt.Printf(" %s", printFunctions[i](dot))
 	}
+	fmt.Println("")
+	fmt.Printf("╰───────────╯\n")
 }
