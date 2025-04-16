b407ee0d083451d34d2f2a228e5063fafed503bb
sid314 <sidaa314@gmail.com>
2025-04-16 16:23:12 +0530

    Added slowPrint

--- print.go
+++ print.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/rivo/uniseg"
 )
 
@@ -32,7 +34,7 @@ func paddedPrintables(printables []PrintableInfo, requiredLength int) []Printabl
 	return printables
 }
 
-func Print() {
+func fastPrint() {
 	config := GetConfig()
 	theme := GenerateTheme(config)
 	printables := config.Printables
@@ -76,6 +78,88 @@ func Print() {
 	theme.border.Printf("  ╰")
 	for range length {
 		theme.border.Printf("─")
+	}
+	theme.border.Printf("╯\n")
+}
+
+func slowPrint() {
+	config := GetConfig()
+	theme := GenerateTheme(config)
+	printables := config.Printables
+	disableColors := config.DisableColors
+	length := largestFieldLength(config.DisableColors, printables)
+	printables = paddedPrintables(printables, length)
+	length = length + 2
+	dot := theme.dot
+	delay := time.Millisecond * 50
+	theme.border.Printf("  ╭")
+	time.Sleep(delay)
+	for range length {
+		theme.border.Printf("─")
+		time.Sleep(delay)
+	}
+	theme.border.Printf("╮\n")
+	time.Sleep(delay)
+	for i, j := 0, 0; i < len(printables); i++ {
+		theme.border.Printf("  │ ")
+		time.Sleep(delay)
+		// theme.colors[j].Print(printables[i].Field)
+		field := uniseg.NewGraphemes(printables[i].Field)
+		for field.Next() {
+			theme.colors[j].Print(string(field.Bytes()))
+			time.Sleep(delay)
+		}
+		time.Sleep(delay)
+		theme.border.Printf("│ ")
+		time.Sleep(delay)
+		value := uniseg.NewGraphemes(printables[i].Value)
+		for value.Next() {
+			theme.colors[j].Print(string(value.Bytes()))
+
+			time.Sleep(delay)
+		}
+		theme.colors[j+1].Println()
+		// time.Sleep(delay)
+		if j >= 14 {
+			j = -2
+		}
+		j += 2
+	}
+	if !disableColors {
+		theme.border.Printf("  ├")
+		time.Sleep(delay)
+		for range length {
+			theme.border.Printf("─")
+			time.Sleep(delay)
+		}
+		theme.border.Printf("┤ \n")
+		time.Sleep(delay)
+		time.Sleep(delay)
+		theme.border.Printf("  │ ")
+		time.Sleep(delay)
+		// theme.colors[0].Printf("󰏘 colors")
+		colorsLabel := uniseg.NewGraphemes("󰏘 colors")
+		for colorsLabel.Next() {
+			theme.colors[0].Print(string(colorsLabel.Bytes()))
+			time.Sleep(delay)
+		}
+		for range length - 9 {
+			theme.border.Print(" ")
+		}
+		theme.border.Printf("│ ")
+		time.Sleep(delay)
+		for i := 0; i < 16; i += 2 {
+			theme.colors[i].Printf("%s ", dot)
+			time.Sleep(delay)
+		}
+		theme.border.Print("\n")
+		time.Sleep(delay)
+	}
+	theme.border.Printf("  ╰")
+	time.Sleep(delay)
+	for range length {
+		theme.border.Printf("─")
+		time.Sleep(delay)
 	}
 	theme.border.Printf("╯\n")
 }
