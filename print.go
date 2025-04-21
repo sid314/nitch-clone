bbdf40a878a7811c9052e9d0ef438da26d0aa27c
sid314 <sidaa314@gmail.com>
2025-04-21 10:53:28 +0530

    Simplified print.go

--- print.go
+++ print.go
@@ -23,66 +23,19 @@ func paddedPrintables(printables []PrintableInfo, requiredLength int) []Printabl
 
 func Print() {
 	config := GetConfig()
+	theme := GenerateTheme(config)
+	printables := config.Printables
+	disableColors := config.DisableColors
+	length := largestFieldLength(config.DisableColors, printables)
+	printables = paddedPrintables(printables, length)
+	length = length + 2
+	dot := theme.dot
+	var delay time.Duration
 	if config.Slow {
-		slowPrint(config)
+		delay = time.Millisecond * 50
 	} else {
-		fastPrint(config)
+		delay = 0
 	}
-}
-
-func fastPrint(config Config) {
-	theme := GenerateTheme(config)
-	printables := config.Printables
-	disableColors := config.DisableColors
-	length := largestFieldLength(config.DisableColors, printables)
-	printables = paddedPrintables(printables, length)
-	length = length + 2
-	dot := theme.dot
-	theme.border.Printf("  ╭")
-	for range length {
-		theme.border.Printf("─")
-	}
-	theme.border.Printf("╮\n")
-	for i, j := 0, 0; i < len(printables); i++ {
-		theme.border.Printf("  │ ")
-		theme.colors[j].Print(printables[i].Field)
-		theme.border.Printf("│ ")
-		theme.colors[j+1].Printf("%s\n", printables[i].Value)
-		j += 2
-	}
-	if !disableColors {
-		theme.border.Printf("  ├")
-		for range length {
-			theme.border.Printf("─")
-		}
-		theme.border.Printf("┤ \n")
-		theme.border.Printf("  │ ")
-		theme.colors[0].Printf("󰸌  colors")
-		for range length - 10 {
-			theme.border.Print(" ")
-		}
-		theme.border.Printf("│ ")
-		for i := 0; i < 16; i += 2 {
-			theme.colors[i].Printf("%s ", dot)
-		}
-		theme.border.Print("\n")
-	}
-	theme.border.Printf("  ╰")
-	for range length {
-		theme.border.Printf("─")
-	}
-	theme.border.Printf("╯\n")
-}
-
-func slowPrint(config Config) {
-	theme := GenerateTheme(config)
-	printables := config.Printables
-	disableColors := config.DisableColors
-	length := largestFieldLength(config.DisableColors, printables)
-	printables = paddedPrintables(printables, length)
-	length = length + 2
-	dot := theme.dot
-	delay := time.Millisecond * 50
 	theme.border.Printf("  ╭")
 	time.Sleep(delay)
 	for range length {
