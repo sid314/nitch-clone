387a19f516f2a375050b5a65898bdefbfeef1a51
sid314 <sidaa314@gmail.com>
2025-04-25 16:57:40 +0530

    fixed "fixed out of bounds thing"

--- print.go
+++ print.go
@@ -77,7 +77,7 @@ func print() {
 		}
 		theme.border.Printf("│ ")
 		time.Sleep(delay)
-		for i := 0; i < len(theme.colors); i += 2 {
+		for i := 0; i < 15; i += 2 {
 			theme.colors[i].Printf("%s ", dot)
 			time.Sleep(delay)
 		}
